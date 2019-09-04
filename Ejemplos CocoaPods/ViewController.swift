39ea5f9f98b0801e43a74e3139c6a8b98548e05e
Academia moviles <academiamoviles@MacBook-Air-AM7.local>
2019-09-03 19:36:01 -0500

    primer commit

---
+++ Ejemplos CocoaPods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejemplos CocoaPods
+//
+//  Created by Academia moviles on 9/3/19.
+//  Copyright © 2019 Academia moviles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Ejemplos CocoaPods/Pods-Ejemplos CocoaPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ejemplos_CocoaPods : NSObject
+@end
+@implementation PodsDummy_Pods_Ejemplos_CocoaPods
+@end
